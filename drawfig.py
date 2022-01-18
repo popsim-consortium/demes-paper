6f163679261023879ea6b7ba48171acd89dd15f7
molpopogen <krthornt@uci.edu>
2022-01-18 13:14:56 -0800

    Update drawfig.py to new demesdraw API
    Progress blocked by grahamgower/demesdraw#83

--- drawfig.py
+++ drawfig.py
@@ -12,8 +12,8 @@ def size_max(graph):
 graph = demes.load("models/IM.yaml")
 w = 0.8 * size_max(graph)
 positions = dict(A=0, X=-w, Y=w)
-ax = demesdraw.utils.get_axes(aspect=1)
-ax = demesdraw.tubes(graph, ax=ax, positions=positions, seed=1, inf_ratio=0.4)
+fig, ax = demesdraw.utils.get_fig_axes(aspect=1)
+ax = demesdraw.tubes(graph) #, ax=ax, positions=positions, seed=1, inf_ratio=0.4)
 ax.figure.savefig(
     "fig/IM.pdf",
     # Save with a transparent background.
